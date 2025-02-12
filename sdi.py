291274df0aa0fb44959c202277c280bd5f47b19c
ks-1220 <yeahkriti30@gmail.com>
2025-02-12 12:11:14 +0530

    Update sdi.py

--- sdi.py
+++ sdi.py
@@ -12,7 +12,7 @@ import numpy as np
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 #from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+#from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
